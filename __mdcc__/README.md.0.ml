e77b3a5940f6d577edf757c9853963053400b952
glennsl <glenn@slotte.net>
2017-05-24 15:55:48 +0200

    Add md.codecheck

---
+++ __mdcc__/README.md.0.ml
@@ -0,0 +1,32 @@
+type line = {
+  start: point;
+  end_: point;
+  thickness: int option
+}
+and point = {
+  x: float;
+  y: float
+}
+
+module Decode = struct
+  let point json =
+    let open! Json.Decode in {
+      x = json |> field "x" float;
+      y = json |> field "y" float
+    }
+
+  let line json =
+    Json.Decode.{
+      start     = json |> field "start" point;
+      end_      = json |> field "end" point;
+      thickness = json |> optional (field "thickness" int)
+    }
+end
+
+let data = {| {
+  "start": { "x": 1.1, "y": -0.4 },
+  "end":   { "x": 5.3, "y": 3.8 }
+} |}
+
+let line = data |> Js.Json.parseExn
+                |> Decode.line
